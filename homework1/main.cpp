0c759328fcf6d3582398774a2448e876c0df6b2c
cutelittlecup <sophiepermyakova1519@gmail.com>
2021-12-22 15:54:01 +0300

    fixed errors in formulas

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -6,7 +6,7 @@ int main() {
 
     double Rightmost_x = 0, Rightmost_y = 0;
     double Leftmost_x = 0, Leftmost_y = 0;
-    double roLastR, roLastL = 0;
+    double roLastR = 0, roLastL = 0;
 
     std::string path = "in.txt";
     std::ifstream DotsFile;
@@ -14,29 +14,36 @@ int main() {
 
     if (DotsFile.is_open()) {
 
-        double A, B;
+        double A, B; // A - коэффициент при y, B - при x: Ax-By=0
         double x_i, y_i;
         double ro;
 
-        DotsFile >> A >> B;
+        DotsFile >> B >> A;
+//
+//        std::cout << "A: "<< A << std::endl;
+//        std::cout << "B: "<< B << std::endl;
 
-//        std::cout << A << std::endl;
-//        std::cout << B << std::endl;
 
         while (!DotsFile.eof()) {
 
             DotsFile >> x_i >> y_i;
 
-            ro = (A * x_i + B * y_i) / sqrt(A*A + B*B);
+            ro = fabs( B * y_i - A * x_i) / sqrt(A * A + B * B);
             ro = round(ro*10000)/10000;
-//            std::cout << ro << std::endl;
+//            std::cout << "ro: "<< ro << std::endl;
+//            std::cout << "rolastR: "<< roLastR << std::endl;
+//            std::cout << "rolastL: "<< roLastL << std::endl;
 
-            if (A*y_i <= B*x_i) {
+
+            if (A*x_i - B * y_i >= 0) {
+
+//                std::cout << "right dot: "<< x_i << " " << y_i << std::endl;
 
                 if (ro >= roLastR) {
 
                     Rightmost_x = x_i;
                     Rightmost_y = y_i;
+//                    std::cout << "Rightmost:" << " "<< Rightmost_x << " " << Rightmost_y << std::endl;
                     roLastR = ro;
                 }
 
@@ -44,9 +51,11 @@ int main() {
             } else {
 
                 if (ro >= roLastL) {
-
+//                    std::cout << "left dot: "<< x_i << " " << y_i << std::endl;
                     Leftmost_x = x_i;
                     Leftmost_y = y_i;
+//                    std::cout << "Leftmost:" << " "<< Leftmost_x << " " << Leftmost_y << std::endl;
+
                     roLastL = ro;
 
                 }
