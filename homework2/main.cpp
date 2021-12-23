7096289535d13cd31220d9e6615914b141c03ebe
cutelittlecup <sophiepermyakova1519@gmail.com>
2021-12-23 03:24:15 +0300

    last try

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -61,46 +61,43 @@ double recurs(std::vector<double> X, double n){
 std::vector<double> y_Coords_Finder(std::vector<double> xCoords, double vx, double vy, double h0, double g, int n, std::vector<double> xCollision){
     std::vector<double> yCoords;
     for(int i = 0; i < xCoords.size(); i++){
-        double y = h0 + pow(-1, n) * (xCoords[i] - recurs(xCollision, n)) * vy / vx - pow((xCoords[i] - recurs(xCollision,n)),2) * g / (2*vx*vx);
+        double y = h0 + pow(-1, n) * (xCoords[i] - recurs(xCollision, n)) * vy / vx - pow((xCoords[i] - recurs(xCollision,n)),2) * g / (2 * vx * vx);
         yCoords.push_back(y);
     }
     return yCoords;
 }
 
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) { //
 
     if (argc == 2) {
 
         std::vector<double> xCollision;
 
         int n = 0;
-        int xCollise;
+        int xCollise = 0;
 
-        std::string path = argv[1];
+        std::string path = argv[1] ; //"homework2.txt"
 
         std::vector<std::string> coords_in_string = FileReader(path);
         std::vector<double> coords = CoordsFinder(coords_in_string);
-
-//        for (int i = 0; i < coords.size(); i++){
-//            std::cout << "coords: "<< coords[i] << std::endl;
-//        }
 
         double g = 9.81;
         double h0 = coords[0];
         double vx = coords[1];
         double vy = coords[2];
 
+//        std::cout << "h0: " << h0 << std::endl;
+//        std::cout << "vx: " << vx << std::endl;
+//        std::cout << "vy: " << vy << std::endl;
+
         //удаляем из массива с координатами константы
         coords.erase(coords.begin() + 0, coords.begin() + 3);
 
-//    std::cout << "h0: " << h0 << std::endl;
-//    std::cout << "vx: " << vx << std::endl;
-//    std::cout << "vy: " << vy << std::endl;
 
         if (coords.size() <= 1) {
 
-            std::cout << 0;
+            std::cout << 0; // Нет стенок
 
         } else {
 
@@ -108,37 +105,39 @@ int main(int argc, char** argv) {
             std::vector<double> hCoords = x_and_h_coords(coords, 1);
             std::vector<double> xCoords = x_and_h_coords(coords, 0);
 
-//         for (int i = 0; i < hCoords.size(); i++) {
-//             std::cout << "hCoords: " << hCoords[i] << std::endl;
-//         }
-//         for (int i = 0; i < xCoords.size(); i++) {
-//             std::cout << "xCoords: " << xCoords[i] << std::endl;
-//         }
-//         std::cout << "xCoords.size: " << xCoords.size() << std::endl;
+//            for (int i = 0; i < hCoords.size(); i++){
+//                std::cout << "xCoords: " << xCoords[i] << std::endl;
+//                std::cout << "hCoords: " << hCoords[i] << std::endl;
+//            }
 
             std::vector<double> yCoords = y_Coords_Finder(xCoords, vx, vy, h0, g, n, xCollision);
 
-//        for (int i = 0; i < yCoords.size(); i++){
-//            std::cout << "yCoords: "<< yCoords[i] << std::endl;
-//        }
+//            for (int i = 0; i < yCoords.size(); i++){
+//                std::cout << "yCoords: " << yCoords[i] << std::endl;
+//            }
 
             //  П Р О В Е Р К А  Н А  П Е Р В О Е  С Т О Л К Н О В Е Н И Е
-
             for (int i = 0; i < xCoords.size(); i++) {
-                if (yCoords[i] < hCoords[i] && yCoords[i] >= 0) {
+
+                if (yCoords[i] < hCoords[i] && yCoords[i] >= 0) {   // Сталкивается
                     xCollision.push_back(xCoords[i]);
                     xCollise = i;
+//                    std::cout << "xCollise: " << xCollise << std::endl;
                     n++;
                     break;
                 }
 
-                if (yCoords[i] < hCoords[i] && yCoords[i] < 0) {
+//                for (int i = 0; i < xCollision.size(); i++){
+//                    std::cout << "xCollision: " << xCollision[i] << std::endl;
+//                }
+//                std::cout << "xCollise: " << xCollise << std::endl;
 
-                    std::cout << i + 1;
+                if (yCoords[i] < hCoords[i] && yCoords[i] < 0) {    // Не сталкивается
+                    std::cout << i;                                                         // ТУТ БЫЛО + 1
                     return 0;
                 }
 
-                if (yCoords[xCoords.size() - 1] > hCoords[xCoords.size() - 1]) {
+                if (yCoords[xCoords.size() - 1] > hCoords[xCoords.size() - 1]) {    // Перелетает все стенки
                     std::cout << xCoords.size();
                     return 0;
                 }
@@ -146,71 +145,79 @@ int main(int argc, char** argv) {
 
             vx = -vx;
 
+//            std::cout << "xCollise: " << xCollise << std::endl;
 
-
-//         std::cout << "vx: "<< vx << std::endl;
-//         std::cout << "n: "<< n << std::endl;
-
-//         for (int i = 0; i < xCollision.size(); i++){
-//            std::cout << "xCollision: "<< xCollision[i] << std::endl;
-//        }
-
-
-
-            // С Т О Л К Н О В Е Н И Я  Е С Т Ь
-
+            // П О С Л Е Д У Ю Щ И Е  С Т О Л К Н О В Е Н И Я
             while (true) {
 
-                if (vx < 0) {
+                if (vx < 0) {   // Летит влево
 
                     yCoords = y_Coords_Finder(xCoords, vx, vy, h0, g, n, xCollision);
 
-                    for (int i = xCollise; i >= 0; i--) {
+//                    for (int i = 0; i < yCoords.size(); i++){
+//                        std::cout << "yCoords: " << yCoords[i] << std::endl;
+//                    }
+
+                    for (int i = xCollise; i >= 0; i--) {   // Сталкивается со стенкой
 
                         if (yCoords[i] < hCoords[i] && yCoords[i] >= 0) {
                             xCollision.push_back(xCoords[i]);
                             xCollise = i;
                             n++;
                             break;
-
                         }
-                        if (yCoords[i] < hCoords[i] && yCoords[i] < 0) {
 
-                            std::cout << i;
+//                        for (int i = 0; i < xCollision.size(); i++){
+//                            std::cout << "yCoords: " << xCollision[i] << std::endl;
+//                        }
+//                        std::cout << "xCollise: " << xCollise << std::endl;
+//                        std::cout << "n: " << n << std::endl;
 
+                        if (yCoords[i] < hCoords[i] && yCoords[i] < 0) {    // Не сталкивается
+                            std::cout << i - 1;
                             return 0;
 
                         }
-                        if (yCoords[i] > hCoords[0]) {
-                            std::cout << i;
+
+                        if (yCoords[i] > hCoords[0]) {  // Перелетает первую стенку -> попадает в 0 зону
+                            std::cout << 0;
+                            return 0;
                         }
 
                     }
+
                     vx = -vx;
                 }
 
-                if (vx > 0) {
+                if (vx > 0) {   // Летит вправо
 
                     yCoords = y_Coords_Finder(xCoords, vx, vy, h0, g, n, xCollision);
 
                     for (int i = xCollise; i > xCoords.size(); i++) {
 
-                        if (yCoords[i] < hCoords[i] && yCoords[i] >= 0) {
-
+                        if (yCoords[i] < hCoords[i] && yCoords[i] >= 0) {   // Сталкивается
                             xCollision.push_back(xCoords[i]);
                             xCollise = i;
                             n++;
                             break;
-
                         }
-                        if (yCoords[i] < hCoords[i] && yCoords[i] < 0) {
-                            std::cout << "<" << i << ">";
+
+                        if (yCoords[i] < hCoords[i] && yCoords[i] < 0) {    // Не сталкивается
+                            std::cout << i;
                             return 0;
                         }
+
+                        if (yCoords[xCoords.size() - 1] > hCoords[xCoords.size() - 1]) {    // Перелетает все стенки
+                            std::cout << xCoords.size();
+                            return 0;
+                        }
+
                     }
+
                     vx = -vx;
                 }
             }
+
 
         }
 
